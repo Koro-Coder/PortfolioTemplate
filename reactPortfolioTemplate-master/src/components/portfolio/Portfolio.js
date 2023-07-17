4db7d5ea0f76a07db1e3378c4d72b3767949fb52
Ashu <abhishekaggarwal2801@gmail.com>
2023-07-17 23:43:54 +0530

    project section updated

--- reactPortfolioTemplate-master/src/components/portfolio/Portfolio.js
+++ reactPortfolioTemplate-master/src/components/portfolio/Portfolio.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React from "react";
 import PortfolioBlock from "./PortfolioBlock";
-import {Box, Grid} from "@mui/material";
-import {info} from "../../info/Info";
+import { Box, Grid } from "@mui/material";
+import { info } from "../../info/Info";
 
 export default function Portfolio() {
-    return (
-        <Box>
-            <Grid container display={'flex'} justifyContent={'center'}>
-                {info.portfolio.map((project, index) => (
-                   <Grid item xs={12} md={6} key={index}>
-                       <PortfolioBlock image={project.image} live={project.live} source={project.source} title={project.title} />
-                   </Grid>
-                ))}
-            </Grid>
-        </Box>
-    );
-};
+  return (
+    <Box>
+      <Grid container display={"flex"} justifyContent={"center"}>
+        {info.portfolio.map((project, index) => (
+          <Grid item xs={12} md={6} key={index}>
+            <div
+              style={{
+                maxWidth: "100%",
+                padding: "30px",
+              }}
+            >
+              <PortfolioBlock
+                image={project.image}
+                live={project.live}
+                source={project.source}
+                title={project.title}
+              />
+            </div>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
+  );
+}
